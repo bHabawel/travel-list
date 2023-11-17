897d6b4c8fc4e50de57ba5311ccd4101df4139b8
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-17 12:33:27 +0800

    added if and conditional rendering in footer

--- src/App.js
+++ src/App.js
@@ -118,14 +118,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats({ items }) {
+  if (items.length === 0) {
+    return (
+      <footer className="stats">
+        <em>Start adding items to your packing list 🚀</em>
+      </footer>
+    );
+  }
+
   const numItems = items.length;
   const numPacked = items.filter((prevItems) => prevItems.packed).length;
   const numPercentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
       <em>
-        👜 You have {numItems} items on your list, and you already packed{" "}
-        {numPacked} ({numPercentage}%)
+        {numPercentage === 100
+          ? "You got everything! You are ready to go ✈️"
+          : `👜 You have ${numItems} items on your list, and you already packed 
+        ${numPacked} (${numPercentage}%)`}
       </em>
     </footer>
   );
