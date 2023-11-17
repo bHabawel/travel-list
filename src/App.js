3f7f08d9e9dad3d4ce0d671b52d915e2436946c3
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-17 16:51:20 +0800

    added a confirm window for the clear list function

--- src/App.js
+++ src/App.js
@@ -22,7 +22,12 @@ function App() {
   }
 
   function handleClearList() {
-    setItems([]);
+    const confirmed = window.confirm(
+      "Are you sure you want to clear the list?"
+    );
+    if (confirmed) {
+      setItems([]);
+    }
   }
 
   return (
