b7389e665962ad891fca98a4fe65b4ce7647541c
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-17 17:00:08 +0800

    fixed importing status of components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Logo from "./components/Logo";
 import Form from "./components/Form";
 import PackingList from "./components/PackingList";
-import { Stats } from "./components/Stats";
+import Stats from "./components/Stats";
 
 function App() {
   const [items, setItems] = useState([]);
